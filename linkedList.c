1162639d64adedcae3734f8e55137d0b212ff8a8
Tao Lin <lin.tao.5711y@gmail.com>
2016-10-09 17:51:08 -0400

    complete all functions but still need to test

--- linkedList.c
+++ linkedList.c
@@ -1,18 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node {int i; struct node *next;};
+struct node {int var; struct node *next;};
 
 void print_list(struct node *n)
 {
   printf("[");
   while( n->next != NULL ){
-    printf("%d, ", n->i);
+    printf("%d, ", n->var);
     n = n->next;
   }
-  printf("%d]\n", n->i);
+  printf("%d]\n", n->var);
+}
+
+struct node * insert_front(struct node *start, int i)
+{
+  struct node *newNode = (struct node *)malloc(sizeof(struct node));;
+  newNode->var = i;
+  newNode->next = start;
+  return newNode;
+}
+
+struct node * free_list(struct node *n)
+{
+  struct node *beginning = n;
+  
+  while(n != NULL){
+    struct node *nextNode = n->next;
+    free(n);
+    n = nextNode;
+  }
+  
+  return beginning;
+}
+
+struct node * Node(int i, struct node *next)
+{
+  struct node *tempNode = (struct node *)malloc(sizeof(struct node));
+  tempNode->var = i;
+  tempNode->next = next;
+  return tempNode;
+}
+
+void testprint_list()
+{
+  printf("____________print_list\n");
+  struct node *node3 = Node(2, NULL);
+  struct node *node2 = Node(1, node3);
+  struct node *node1 = Node(0, node2);
+  print_list(node1);
+  free_list(node1);
+  printf("\n");
+}
+
+void testinsert_front()
+{
+  printf("____________insert_front\n");
+  struct node *node4 = Node(3, NULL);
+  struct node *node3 = Node(2, node4);
+  struct node *node2 = Node(1, node3);
+  struct node *node1 = Node(0, node2);
+  print_list(node1);
+  free_list(node1);
 }
 
 
-int main(){
+int main()
+{
+  testprint_list();
   return 0;
 }
